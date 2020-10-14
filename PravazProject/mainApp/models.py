5d540e0bd738d67fd841abc5ca24313817dcc81a
ypaovr <ypaolavar@gmail.com>
2020-10-14 11:59:03 -0500

    actualización

--- PravazProject/mainApp/models.py
+++ PravazProject/mainApp/models.py
@@ -1,6 +1,30 @@
 from django.db import models
 
-# Create your models here.
 
-class Medico(models.Model):
-    id = models.AutoField(primary_key)
+class Medico(models.Model):  
+
+    id = models.AutoField(primary_key=True)
+    contraseña = = models.charField(max_length = 100)
+    nombre = models.CharField(max_length= 100)
+    apellidos = models.charField(max_length = 100)
+    telefono = models.charField(max_length = 100)
+    correo = = models.EmailField(max_length=100)
+    fecha_nacimiento = models.DateField()
+
+class Paciente(models.Model):
+    id = models.AutoField(primary_key=True)
+
+class HistoriaClinica(models.Model):
+    id = models.AutoField(primary_key=True)
+
+class Diagnostico(models.Model):
+    id = models.AutoField(primary_key=True)
+
+class Interaccion(models.Model):
+    id = models.AutoField(primary_key=True)
+
+class Solicitud_grupoFam(models.Model):
+    id = models.AutoField(primary_key=True)
+
+class Paciente(models.Model):
+    id = models.AutoField(primary_key=True)
